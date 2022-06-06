1f7293504e7306846c767b8924685460328ea010
Yucel Ismail <ismailycel@gmail.com>
2022-06-06 22:27:33 +0300

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Hamburger, Logo, Nav, Menu, MenuLink } from "./NavbarStyles";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [open, setOpen] = useState(false);
+  const openHamburger = () => setOpen(!open);
+  return (
+    <Nav>
+      <Logo to="/home" onClick={() => setOpen(false)}>
+        <i>{"< Awkward />"}</i>
+        <span> recipe</span>
+      </Logo>
+      <Hamburger onClick={openHamburger}>
+        {/* <span />
+        <span />
+        <span /> */}
+        <GiHamburgerMenu />
+      </Hamburger>
+      <Menu sendOpen={open}>
+        <MenuLink onClick={openHamburger} to="/about">
+          About
+        </MenuLink>
+        <MenuLink onClick={openHamburger} to="/github">
+          Github
+        </MenuLink>
+        <MenuLink onClick={openHamburger} to="/">
+          Logout
+        </MenuLink>
+      </Menu>
+    </Nav>
+  );
 };
 
 export default Navbar;
